44ab34ef39f5470909ca0b53c2ed1a5136a07a2f
Matt Hand <matt@matterpreter.com>
2019-04-09 21:22:03 -0400

    Quick maths
    Fixed the math behind the overshoot/halfsplit logic

--- DefenderCheck/DefenderCheck/Program.cs
+++ DefenderCheck/DefenderCheck/Program.cs
@@ -13,19 +13,21 @@ namespace DefenderCheck
         static void Main(string[] args)
         {
             //Initial file parse
-            string targetfile = args[0];
+            string targetfile = @"C:\Temp\lorem.txt";
             string testfilepath = @"C:\Temp\testfile.txt";
             string detectionStatus = null;
             byte[] originalfilecontents = File.ReadAllBytes(targetfile);
             int originalfilesize = originalfilecontents.Length;
             Console.WriteLine("Target file size: {0} bytes", originalfilecontents.Length);
 
-            byte[] splitarray1 = new byte[originalfilesize/2];
+            byte[] splitarray1 = new byte[originalfilesize];
+            splitarray1 = HalfSplitter(originalfilecontents);
             byte[] splitarray2 = new byte[originalfilesize]; //writing to this buffer fixes it, but we can't use this buffer. We probably need to instantiate the first buffer and then just work from there
-            Array.Copy(originalfilecontents, splitarray1, splitarray1.Length);
+            //Array.Copy(originalfilecontents, splitarray1, splitarray1.Length);
 
             while (splitarray1.Length > 200)
             {
+                //Array.Copy(splitarray2, splitarray1, splitarray2.Length);
                 Console.WriteLine("Testing {0} bytes", splitarray1.Length);
                 File.WriteAllBytes(testfilepath, splitarray1);
                 detectionStatus = Scan(testfilepath).ToString();
@@ -38,8 +40,11 @@ namespace DefenderCheck
                 else if (detectionStatus.Equals("NoThreatFound"))
                 {
                     Console.WriteLine("No threat found. Going up 50% of current size.");
-                    byte[] temparray = Overshot(originalfilecontents, splitarray1.Length);
-                    Buffer.BlockCopy(temparray, 0, splitarray2, 0, temparray.Length);
+                    byte[] temparray = Overshot(originalfilecontents, splitarray1.Length); //Create temp array with 1.5x more byers
+                    //Array.Clear(splitarray1, 0, splitarray1.Length);
+                    
+                    Array.Resize(ref splitarray1, temparray.Length);
+                    Buffer.BlockCopy(temparray, 0, splitarray1, 0, temparray.Length);
                 }
             }
 
@@ -56,7 +61,8 @@ namespace DefenderCheck
 
         public static byte[] Overshot(byte[] originalarray, int splitarraysize)
         {
-            int newsize = (splitarraysize * 3)/2; //Lazy math to get 150% because of double/int syntax ugliness
+            //int newsize = (splitarraysize * 3)/2; //Lazy math to get 150% because of double/int syntax ugliness
+            int newsize = (originalarray.Length - splitarraysize) / 2 + splitarraysize;
             Console.WriteLine("newsize: {0}", newsize);
             byte[] newarray = new byte[newsize];
             Buffer.BlockCopy(originalarray, 0, newarray, 0, newarray.Length);
